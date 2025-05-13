88372367135dd1fb85f4f1219334b6b72c7a3a22
Armand Collin <armac@videotron.ca>
2025-05-13 14:04:31 -0400

    Add model URLs and TEM2 testing set

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,7 +3,7 @@ from dandi.download import download
 
 
 class ASTIHDataset:
-    def __init__(self, name, id, url, desc, test_set):
+    def __init__(self, name, id, url, desc, test_set, model_url):
 
         self.name = name
         self.dandi_id = id
@@ -17,6 +17,7 @@ class ASTIHDataset:
             self.test_set_url = test_set
         else:
             raise ValueError("test_set must be a list of subjects or a URL")
+        self.model_release = model_url
 
 DATASETS = [
     ASTIHDataset(
@@ -26,14 +27,16 @@ DATASETS = [
         desc="TEM Images of Corpus Callosum in Control and Cuprizone-Intoxicated Mice with Axon and Myelin Segmentations",
         test_set=[
             "sub-nyuMouse26"
-        ]
+        ],
+        model_url="https://github.com/axondeepseg/default-TEM-model/releases/download/r20240403/model_seg_mouse_axon-myelin_tem_light.zip"
     ),
     ASTIHDataset(
         name="TEM2",
         id="001350",
         url="https://dandiarchive.org/dandiset/001350/0.250511.1527",
         desc="TEM Images of Corpus Callosum in Flox/SRF-cKO Mice",
-        test_set="INSERT_EXTERNAL_URL_HERE"
+        test_set="https://github.com/axondeepseg/data_axondeepseg_srf_testing/archive/refs/tags/r20250513-neurips2025.zip",
+        model_url="https://github.com/axondeepseg/model_seg_unmyelinated_tem/releases/download/r20240708-stanford/model_seg_unmyelinated_stanford_tem_best.zip"
     ),
     ASTIHDataset(
         name="SEM1",
@@ -42,7 +45,8 @@ DATASETS = [
         desc="SEM Images of Rat Spinal Cord with Axon and Myelin Segmentations with Myelinated and Unmyelinated Axon Segmentations",
         test_set=[
             "sub-rat6"
-        ]
+        ],
+        model_url="https://github.com/axondeepseg/default-SEM-model/releases/download/r20240403/model_seg_rat_axon-myelin_sem_light.zip"
     ),
     ASTIHDataset(
         name="BF1",
@@ -52,7 +56,8 @@ DATASETS = [
         test_set=[
             "sub-rat2",
             "sub-rat7"
-        ]
+        ],
+        model_url="https://github.com/axondeepseg/default-BF-model/releases/download/r20240405/model_seg_rat_axon-myelin_bf_light.zip"
     )
 ]
 

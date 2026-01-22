d827aec031068d0f127befe93caeefb938bc4fd2
Armand Collin <armac@videotron.ca>
2026-01-22 11:42:40 -0500

    Add evaluation for cellpose pipeline

--- scripts/evaluate_models.py
+++ scripts/evaluate_models.py
@@ -5,8 +5,10 @@ import cv2
 import numpy as np
 import pandas as pd
 import warnings
+import argparse
 from skimage import measure
 from AxonDeepSeg.morphometrics.compute_morphometrics import get_watershed_segmentation
+from AxonDeepSeg.ads_utils import imread
 from stardist.matching import matching
 
 from get_data import load_datasets
@@ -88,7 +90,6 @@ def apply_watershed(ax_mask, my_mask):
 
     return get_watershed_segmentation(ax_mask, my_mask, centroids)
 
-
 def extract_binary_masks(mask):
     '''
     This function will take as input an 8-bit image containing both the axon 
@@ -104,8 +105,8 @@ def extract_binary_masks(mask):
     axon_mask = torch.from_numpy(axon_mask).float()
     return axon_mask, myelin_mask
 
-def main():
-    metrics = [DiceMetric(), MeanIoU()] #, PanopticQualityMetric(num_classes=1)]
+def main(eval_cellpose: bool = False, matching_thresh: float = 0.5):
+    metrics = [DiceMetric(), MeanIoU()]
     metric_names = [metric.__class__.__name__ for metric in metrics]
     columns = ['dataset', 'image', 'class'] + metric_names
     pixelwise_df = pd.DataFrame(columns=columns)
@@ -113,6 +114,8 @@ def main():
     detection_df = pd.DataFrame(columns=columns_detection)
     
     data_splits_path = Path("data/splits")
+    if eval_cellpose:
+        cellpose_path = Path("data/cellpose_pipeline")
     assert data_splits_path.exists(), "Data splits directory does not exist. Please run get_data.py with --make-splits arg first."
     
     datasets = load_datasets()
@@ -140,7 +143,7 @@ def main():
             gt_im = np.floor(gt_im / np.max(gt_im) * 255).astype(np.uint8)
             gt_ax, gt_my = extract_binary_masks(gt_im)
 
-            # load predictions
+            # load axon and myelin predictions
             ax_pred = cv2.imread(str(testset_path / ax_pred_fname), cv2.IMREAD_GRAYSCALE)[None]
             ax_pred = np.floor(ax_pred / np.max(ax_pred) * 255).astype(np.uint8)
             ax_pred, _ = extract_binary_masks(ax_pred)
@@ -148,6 +151,39 @@ def main():
             my_pred = np.floor(my_pred / np.max(my_pred) * 255).astype(np.uint8)
             my_pred, _ = extract_binary_masks(my_pred)
 
+            # INSTANCE-WISE EVALUATION
+            if eval_cellpose:
+                cp_pred_fname = gt.name.replace("_seg-axonmyelin-manual.png", "_cp_masks.png")
+                cp_pred_path = cellpose_path / f'cellpose_preprocessed_{dset.name}' / 'test' / cp_pred_fname
+                assert cp_pred_path.exists(), f"Cellpose predictions not found for {img_fname}"
+                inst_pred = imread(cp_pred_path, use_16bit=True)
+
+                inst_gt_fname = cp_pred_fname.replace('_cp_masks.png', '_seg-cellpose.png')
+                inst_gt_path = cellpose_path / f'cellpose_preprocessed_{dset.name}' / 'test' / inst_gt_fname
+                inst_gt = imread(inst_gt_path, use_16bit=True)
+            else:
+                inst_pred = apply_watershed(ax_pred, my_pred)
+                inst_gt = apply_watershed(gt_ax, gt_my)
+
+            stats = matching(inst_gt, inst_pred, thresh=matching_thresh)
+            detection_row = {
+                'dataset':      dset.name,
+                'image':        img_fname,
+                'TP':           stats.tp,
+                'FP':           stats.fp, 
+                'FN':           stats.fn, 
+                'precision':    stats.precision,
+                'recall':       stats.recall,
+                'accuracy':     stats.accuracy,
+                'f1':           stats.f1
+            }
+            print(f'detection metrics: {detection_row}')
+            detection_df = pd.concat([detection_df, pd.DataFrame([detection_row])], ignore_index=True)
+
+            if eval_cellpose:
+                continue
+
+            # PIXEL-WISE EVALUATION
             classwise_pairs = [
                 ('axon', ax_pred, gt_ax),
                 ('myelin', my_pred, gt_my)
@@ -165,31 +201,25 @@ def main():
                     row[metric.__class__.__name__] = value
                 pixelwise_df = pd.concat([pixelwise_df, pd.DataFrame([row])], ignore_index=True)
 
-            # compute detection metrics
-            inst_gt = apply_watershed(gt_ax, gt_my)
-            inst_pred = apply_watershed(ax_pred, my_pred)
-            stats = matching(inst_gt, inst_pred, thresh=0.3)
-            detection_row = {
-                'dataset':      dset.name,
-                'image':        img_fname,
-                'TP':           stats.tp,
-                'FP':           stats.fp, 
-                'FN':           stats.fn, 
-                'precision':    stats.precision,
-                'recall':       stats.recall,
-                'accuracy':     stats.accuracy,
-                'f1':           stats.f1
-            }
-            print(f'detection metrics: {detection_row}')
-            detection_df = pd.concat([detection_df, pd.DataFrame([detection_row])], ignore_index=True)
-
     # Export the dataframe to a CSV file
-    pixelwise_df.to_csv('metrics.csv', index=False)
-    print("Metrics computed and saved to metrics.csv")
-    detection_df.to_csv('det_metrics.csv', index=False)
-    print("Detection metrics computed and saved to det_metrics.csv")
-    print(aggregate_det_metrics(detection_df))
+    if eval_cellpose:
+        detection_df.to_csv('cellpose_det_metrics.csv', index=False)
+        print("Cellpose detection metrics computed and saved to cellpose_det_metrics.csv")
+        print(aggregate_det_metrics(detection_df))
+    else:
+        pixelwise_df.to_csv('metrics.csv', index=False)
+        print("Metrics computed and saved to metrics.csv")
+        detection_df.to_csv('det_metrics.csv', index=False)
+        print("Detection metrics computed and saved to det_metrics.csv")
+        print(aggregate_det_metrics(detection_df))
 
 
 if __name__ == "__main__":
-    main()
+    ap = argparse.ArgumentParser(description="Evaluate segmentation models")
+    ap.add_argument(
+        '--eval_cellpose', 
+        action='store_true', 
+        default=False, 
+        help="Evaluate Cellpose model predictions. Will only run the object detection evaluation. Expects predictions with suffix '_cp_masks'.")
+    args = ap.parse_args()
+    main(args.eval_cellpose)
